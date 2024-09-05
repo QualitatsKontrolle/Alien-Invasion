a03b9c5933ed1f769ca56dd83a105d3340efbef3
QualitatsKontrolle <katua-wambua@protonmail.com>
2024-09-05 10:30:59 +0300

    bullet class - manage bullets fired from ship

--- game_functions.py
+++ game_functions.py
@@ -1,11 +1,17 @@
 import sys, pygame
+from bullet import Bullet
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, game_settings, screen, ship, bullets):
     """Respond to keypresses"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        # Create a new bullet & add it to the bullets group
+        if len(bullets) < game_settings.bullets_allowed:
+            new_bullet = Bullet(game_settings, screen, ship)
+            bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
     """Respond to key releases"""
@@ -14,17 +20,26 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ship):
+def check_events(game_settings, screen, ship, bullets):
     """Respond to keypresses & mouse events"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, game_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
+        elif event.type == pygame.K_SPACE:
+            fire_bullet(game_settings, screen, ship, bullets)
 
-def update_screen(game_settings, screen, ship):
+def fire_bullet(game_settings, screen, ship, bullets):
+    """Fire a bullet if limit not reached yet"""
+    # Create a new bullet & add it to the bullets group
+    if len(bullets) < game_settings.bullets_allowed:
+        new_bullet = Bullet(game_settings, screen, ship)
+        bullets.add(new_bullet)
+
+def update_screen(game_settings, screen, ship, bullets):
     """Update images on screen & flip to new screen."""
     # Redraw screen during each pass through the loop
     screen.fill(game_settings.bg_color)
@@ -32,3 +47,18 @@ def update_screen(game_settings, screen, ship):
     
     # Make most recently drawn screen visible
     pygame.display.flip()
+    
+    # Redraw all bullets behind ship & aliens
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+
+def update_bullets(bullets):
+    """Update position of bullets & get rid of old ones"""
+    # Update bullet positions
+    bullets.update()
+    
+    # Get rid of bullets that have disappeared
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 os.environ["SDL_VIDEODRIVER"]="x11"
 
 import pygame, sys
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -20,6 +21,8 @@ def run_game():
     
     # Make a ship
     ship = Ship(game_settings, screen)
+    # Make group to store bullets in
+    bullets = Group()
 
     # Set background color
     bg_color = (230, 230, 230)
@@ -30,9 +33,17 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        gf.check_events(ship)
+        gf.check_events(game_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(game_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(game_settings, screen, ship, bullets)
+        
+        # Get rid of bullets that have disappeared
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+        
+        gf.update_screen(game_settings, screen, ship, bullets)
      
         # Redraw screen during each pass through the loop
         screen.fill(game_settings.bg_color)
